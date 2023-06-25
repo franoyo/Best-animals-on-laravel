b66cdd15998b9dd2fed9cec051c714f174050bc9
franoyo <oyola294@gmail.com>
2023-06-25 18:18:39 -0500

    a

--- resources/views/pdf/pdf_empleado.blade.php
+++ resources/views/pdf/pdf_empleado.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Informe Empleados {{$fecha}}</title>
-    <link rel="stylesheet" href="{{asset('css/stylePDFClientes.css?v=1.32')}}">
+    <link rel="stylesheet" href="{{asset('css/stylePDFClientes.css?v=1.35')}}">
 </head>
 <body>
    
