fefd7b449553e8bc8ffc9fa9c72ed2b158fdaf1b
franoyo <127349021+franoyo@users.noreply.github.com>
2024-03-15 11:46:37 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if($this->app->environment('production')) {
+        \URL::forceScheme('https');
+    }
         Validator::extend('custom_email', function ($attribute, $value, $parameters, $validator) {
             return preg_match('/^.+@.+\.[A-Za-z]{2,}$/i', $value);
         });
