997c3942d3b34334c9ed36bd2b8d598a73a76c37
franoyo <127349021+franoyo@users.noreply.github.com>
2024-03-15 13:54:02 -0500

    Update guarderia.blade.php

--- resources/views/guarderia.blade.php
+++ resources/views/guarderia.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guarderia</title>
-    <link rel="stylesheet" href="CSS/style17.css">
+    <link rel="stylesheet" href="css/style17.css">
 </head>
 <body>
     <div class="general-container">
