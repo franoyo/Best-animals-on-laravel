d177b8297b2d169fd3f3dbe5518c5449873fa205
franoyo <oyola294@gmail.com>
2024-03-18 18:32:53 -0500

    try fix

--- resources/views/alerts/alert_eliminar.blade.php
+++ resources/views/alerts/alert_eliminar.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('CSS/style23.css?v=1.0')}}">
+    <link rel="stylesheet" href="{{asset('css/style23.css?v=1.49')}}">
     <title>alerta</title>
 </head>
 <body>
